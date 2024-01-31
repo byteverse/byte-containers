b6af665e147240f9dd3c5c2761cd44575fb8a4f2
Brian McKeon <bmckeon@layer3com.com>
2024-01-31 09:50:10 -0500

    Prepare for 0.1.0.0 release.

--- src/Data/Map/Word8.hs
+++ src/Data/Map/Word8.hs
@@ -1,9 +1,9 @@
-{-# language BangPatterns #-}
-{-# language BlockArguments #-}
-{-# language DeriveFunctor #-}
-{-# language DerivingStrategies #-}
-{-# language StandaloneDeriving #-}
-{-# language TypeApplications #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Data.Map.Word8
   ( Map
@@ -26,7 +26,7 @@ module Data.Map.Word8
 import Prelude hiding (lookup, null)
 
 import Control.Monad.ST.Run (runSmallArrayST)
-import Data.Bits (testBit,bit,unsafeShiftR,(.&.),(.|.),popCount)
+import Data.Bits (bit, popCount, testBit, unsafeShiftR, (.&.), (.|.))
 import Data.Primitive (SmallArray)
 import Data.WideWord (Word256)
 import Data.Word (Word8)
@@ -35,26 +35,29 @@ import qualified Data.Foldable as F
 import qualified Data.Primitive as PM
 
 -- | A map whose keys are 8-bit words.
-data Map a = Map
-  -- Invariant: len(values) = popcnt keys
-  {-# UNPACK #-} !Word256
-  {-# UNPACK #-} !(SmallArray a)
+data Map a
+  = Map
+      -- Invariant: len(values) = popcnt keys
+      {-# UNPACK #-} !Word256
+      {-# UNPACK #-} !(SmallArray a)
 
-deriving stock instance Eq a => Eq (Map a)
+deriving stock instance (Eq a) => Eq (Map a)
 deriving stock instance Functor Map
 
-instance Show a => Show (Map a) where
+instance (Show a) => Show (Map a) where
   showsPrec p m = showsPrec p (toList m)
 
-instance Semigroup a => Semigroup (Map a) where
+instance (Semigroup a) => Semigroup (Map a) where
   (<>) = unionWith (<>)
 
-instance Semigroup a => Monoid (Map a) where
+instance (Semigroup a) => Monoid (Map a) where
   mempty = empty
 
 singleton :: Word8 -> a -> Map a
-singleton !k v = Map (bit (fromIntegral @Word8 @Int k))
-  (runSmallArrayST (PM.newSmallArray 1 v >>= PM.unsafeFreezeSmallArray))
+singleton !k v =
+  Map
+    (bit (fromIntegral @Word8 @Int k))
+    (runSmallArrayST (PM.newSmallArray 1 v >>= PM.unsafeFreezeSmallArray))
 
 -- | Is the passed map empty?
 null :: Map a -> Bool
@@ -74,15 +77,16 @@ lookup kw (Map keys vals) = case testBit keys k of
   False -> Nothing
   True -> case k of
     0 -> Just (PM.indexSmallArray vals 0)
-    _ -> 
+    _ ->
       let ix = popCount (unsafeShiftR maxBound (256 - k) .&. keys)
        in Just (PM.indexSmallArray vals ix)
-  where
+ where
   k = fromIntegral @Word8 @Int kw
 
--- | The expression @'union' t1 t2@ takes the left-biased union
--- of @t1@ and @t2@. It prefers @t1@ when duplicate keys are
--- encountered (i.e. @'union' == 'unionWith' const@).
+{- | The expression @'union' t1 t2@ takes the left-biased union
+of @t1@ and @t2@. It prefers @t1@ when duplicate keys are
+encountered (i.e. @'union' == 'unionWith' const@).
+-}
 union :: Map a -> Map a -> Map a
 union !ma@(Map ksA vsA) !mb@(Map ksB vsB)
   | ksA == 0 = mb
@@ -91,19 +95,22 @@ union !ma@(Map ksA vsA) !mb@(Map ksB vsB)
       let sz = popCount ks
       dst <- PM.newSmallArray sz =<< PM.indexSmallArrayM vsA 0
       foldlZipBits256
-        ( \(!ix,!ixA,!ixB) a b -> case a of
-          True -> do
-            PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsA ixA
-            pure (ix + 1, ixA + 1, if b then ixB + 1 else ixB)
-          False -> case b of
+        ( \(!ix, !ixA, !ixB) a b -> case a of
             True -> do
-              PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsB ixB
-              pure (ix + 1, ixA, ixB + 1)
-            False -> pure (ix, ixA, ixB)
-        ) (0,0,0) ksA ksB
+              PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsA ixA
+              pure (ix + 1, ixA + 1, if b then ixB + 1 else ixB)
+            False -> case b of
+              True -> do
+                PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsB ixB
+                pure (ix + 1, ixA, ixB + 1)
+              False -> pure (ix, ixA, ixB)
+        )
+        (0, 0, 0)
+        ksA
+        ksB
       PM.unsafeFreezeSmallArray dst
-      where
-      ks = ksA .|. ksB
+ where
+  ks = ksA .|. ksB
 
 -- | Union with a combining function.
 unionWith :: (a -> a -> a) -> Map a -> Map a -> Map a
@@ -114,43 +121,52 @@ unionWith g !ma@(Map ksA vsA) !mb@(Map ksB vsB)
       let sz = popCount ks
       dst <- PM.newSmallArray sz =<< PM.indexSmallArrayM vsA 0
       foldlZipBits256
-        ( \(!ix,!ixA,!ixB) a b -> case a of
-          True -> case b of
-            True -> do
-              a <- PM.indexSmallArrayM vsA ixA
-              b <- PM.indexSmallArrayM vsB ixB
-              let !c = g a b
-              PM.writeSmallArray dst ix c
-              pure (ix + 1, ixA + 1, ixB + 1)
-            False -> do
-              PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsA ixA
-              pure (ix + 1, ixA + 1, ixB)
-          False -> case b of
-            True -> do
-              PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsB ixB
-              pure (ix + 1, ixA, ixB + 1)
-            False -> pure (ix, ixA, ixB)
-        ) (0,0,0) ksA ksB
+        ( \(!ix, !ixA, !ixB) a b -> case a of
+            True -> case b of
+              True -> do
+                a <- PM.indexSmallArrayM vsA ixA
+                b <- PM.indexSmallArrayM vsB ixB
+                let !c = g a b
+                PM.writeSmallArray dst ix c
+                pure (ix + 1, ixA + 1, ixB + 1)
+              False -> do
+                PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsA ixA
+                pure (ix + 1, ixA + 1, ixB)
+            False -> case b of
+              True -> do
+                PM.writeSmallArray dst ix =<< PM.indexSmallArrayM vsB ixB
+                pure (ix + 1, ixA, ixB + 1)
+              False -> pure (ix, ixA, ixB)
+        )
+        (0, 0, 0)
+        ksA
+        ksB
       PM.unsafeFreezeSmallArray dst
-      where
-      ks = ksA .|. ksB
+ where
+  ks = ksA .|. ksB
 
 insert :: Word8 -> a -> Map a -> Map a
 insert = insertWith const
 
--- | Insert with a function, combining new value and old value.
--- @'insertWith' f key value mp@ will insert the pair @(key, value)@ into @mp@
--- if @key@ does not exist in the map.
--- If the key does exist, the function will insert the pair
--- @(key, f new_value old_value)@.
+{- | Insert with a function, combining new value and old value.
+@'insertWith' f key value mp@ will insert the pair @(key, value)@ into @mp@
+if @key@ does not exist in the map.
+If the key does exist, the function will insert the pair
+@(key, f new_value old_value)@.
+-}
 insertWith :: (a -> a -> a) -> Word8 -> a -> Map a -> Map a
 insertWith f k v m = unionWith f (singleton k v) m
 
 -- Internal function. This is strict in the accumulator.
-foldlZipBits256 :: Monad m
-  => (a -> Bool -> Bool -> m a) -> a -> Word256 -> Word256 -> m ()
+foldlZipBits256 ::
+  (Monad m) =>
+  (a -> Bool -> Bool -> m a) ->
+  a ->
+  Word256 ->
+  Word256 ->
+  m ()
 foldlZipBits256 g !a0 !x !y = go 0 a0
-  where
+ where
   go !ix !a = case ix of
     256 -> pure ()
     _ -> do
@@ -161,7 +177,7 @@ foldlZipBits256 g !a0 !x !y = go 0 a0
 
 foldrBits256 :: (Word8 -> b -> b) -> b -> Word256 -> b
 foldrBits256 g b0 w = go 0
-  where
+ where
   go ix = case ix of
     256 -> b0
     _ -> case testBit w ix of
@@ -170,26 +186,27 @@ foldrBits256 g b0 w = go 0
 
 foldrWithKeys :: (Word8 -> a -> b -> b) -> b -> Map a -> b
 foldrWithKeys g b0 (Map ks vs) = go 0 0
-  where
+ where
   go !ix !ixVal = case ix of
     256 -> b0
     _ -> case testBit ks ix of
-      True -> g
-        (fromIntegral @Int @Word8 ix)
-        (PM.indexSmallArray vs ixVal)
-        (go (ix + 1) (ixVal + 1))
+      True ->
+        g
+          (fromIntegral @Int @Word8 ix)
+          (PM.indexSmallArray vs ixVal)
+          (go (ix + 1) (ixVal + 1))
       False -> go (ix + 1) ixVal
 
 foldl' :: (b -> a -> b) -> b -> Map a -> b
-{-# inline foldl' #-}
+{-# INLINE foldl' #-}
 foldl' f b0 (Map _ vs) = F.foldl' f b0 vs
 
-traverse_ :: Applicative m => (a -> m b) -> Map a -> m ()
-{-# inline traverse_ #-}
+traverse_ :: (Applicative m) => (a -> m b) -> Map a -> m ()
+{-# INLINE traverse_ #-}
 traverse_ f (Map _ vs) = F.traverse_ f vs
 
-toList :: Map a -> [(Word8,a)]
-toList = foldrWithKeys (\k v b -> (k,v) : b) []
+toList :: Map a -> [(Word8, a)]
+toList = foldrWithKeys (\k v b -> (k, v) : b) []
 
-fromList :: [(Word8,a)] -> Map a
-fromList = F.foldl' (\acc (k,v) -> union acc (singleton k v)) empty
+fromList :: [(Word8, a)] -> Map a
+fromList = F.foldl' (\acc (k, v) -> union acc (singleton k v)) empty
--- test/Main.hs
+++ test/Main.hs
@@ -1,21 +1,19 @@
-{-# language BangPatterns #-}
-{-# language MultiWayIf #-}
-{-# language OverloadedStrings #-}
-{-# language ScopedTypeVariables #-}
-{-# language TypeApplications #-}
-
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-import Data.Proxy (Proxy(..))
 import Data.Map.Word8 (Map)
+import Data.Proxy (Proxy (..))
 import Data.Semigroup (Sum)
 import Data.Word (Word8)
-import Test.Tasty (defaultMain,testGroup,TestTree)
-import Test.Tasty.QuickCheck ((===),testProperty,property)
-import Test.Tasty.QuickCheck (Arbitrary,Discard(Discard))
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.QuickCheck (Arbitrary, Discard (Discard), property, testProperty, (===))
 
-import qualified Data.Map.Word8 as Map
 import qualified Data.List as List
+import qualified Data.Map.Word8 as Map
 import qualified Test.QuickCheck.Classes as QCC
 import qualified Test.Tasty.QuickCheck as TQC
 
@@ -23,53 +21,56 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "byte-containers"
-  [ testGroup "append"
-    [ testProperty "doubleton" $ \ka (va :: Word) kb vb ->
-        Map.toList
-          (Map.union (Map.singleton ka va) (Map.singleton kb vb))
-        ===
-        (case compare ka kb of
-           EQ -> [(ka,va)]
-           LT -> [(ka,va),(kb,vb)]
-           GT -> [(kb,vb),(ka,va)]
-        )
-    , testProperty "lookup" lookupProp
-    , testProperty "associative" $ \alist blist clist ->
-        let a = Map.fromList alist :: Map Word
-            b = Map.fromList blist
-            c = Map.fromList clist
-         in Map.union a (Map.union b c)
-            ===
-            Map.union (Map.union a b) c
-    , lawsToTest (QCC.semigroupLaws (Proxy :: Proxy (Map [Integer])))
-    , lawsToTest (QCC.monoidLaws (Proxy :: Proxy (Map [Integer])))
-    , lawsToTest (QCC.commutativeMonoidLaws (Proxy :: Proxy (Map (Sum Integer))))
-    , lawsToTest (QCC.functorLaws (Proxy :: Proxy Map))
+tests =
+  testGroup
+    "byte-containers"
+    [ testGroup
+        "append"
+        [ testProperty "doubleton" $ \ka (va :: Word) kb vb ->
+            Map.toList
+              (Map.union (Map.singleton ka va) (Map.singleton kb vb))
+              === ( case compare ka kb of
+                      EQ -> [(ka, va)]
+                      LT -> [(ka, va), (kb, vb)]
+                      GT -> [(kb, vb), (ka, va)]
+                  )
+        , testProperty "lookup" lookupProp
+        , testProperty "associative" $ \alist blist clist ->
+            let a = Map.fromList alist :: Map Word
+                b = Map.fromList blist
+                c = Map.fromList clist
+             in Map.union a (Map.union b c)
+                  === Map.union (Map.union a b) c
+        , lawsToTest (QCC.semigroupLaws (Proxy :: Proxy (Map [Integer])))
+        , lawsToTest (QCC.monoidLaws (Proxy :: Proxy (Map [Integer])))
+        , lawsToTest (QCC.commutativeMonoidLaws (Proxy :: Proxy (Map (Sum Integer))))
+        , lawsToTest (QCC.functorLaws (Proxy :: Proxy Map))
+        ]
+    , testGroup
+        "insert"
+        [ testProperty "replaces" $ \k v v' alist ->
+            let a = Map.insert k v (Map.fromList alist) :: Map Word
+                a' = Map.insert k v' a
+             in if
+                  | v /= v' ->
+                      Map.lookup k a' === Just v'
+                  | otherwise -> property Discard
+        ]
     ]
-  , testGroup "insert"
-    [ testProperty "replaces" $ \k v v' alist ->
-      let a = Map.insert k v (Map.fromList alist) :: Map Word
-          a' = Map.insert k v' a
-        in if | v /= v'
-                -> Map.lookup k a' === Just v'
-              | otherwise -> property Discard
-    ]
-  ]
 
-instance Arbitrary a => Arbitrary (Map a) where
+instance (Arbitrary a) => Arbitrary (Map a) where
   arbitrary = Map.fromList <$> TQC.arbitrary
 
 lookupProp :: TQC.Property
-lookupProp = TQC.property $ \(xs :: [(Word8,Integer)]) ->
+lookupProp = TQC.property $ \(xs :: [(Word8, Integer)]) ->
   let ys = Map.fromList xs
    in foldr
-        (\x r -> case List.lookup x xs == Map.lookup x ys of
-          True -> r
-          False -> TQC.counterexample ("key " ++ show x) False
+        ( \x r -> case List.lookup x xs == Map.lookup x ys of
+            True -> r
+            False -> TQC.counterexample ("key " ++ show x) False
         )
         (TQC.property True)
-        [0..255 :: Word8]
+        [0 .. 255 :: Word8]
 
 lawsToTest :: QCC.Laws -> TestTree
 lawsToTest (QCC.Laws name pairs) = testGroup name (map (uncurry TQC.testProperty) pairs)
